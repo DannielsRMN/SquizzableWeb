151245873d9b035c0bc0ef3837ec53f3a5d43b2b
DannielsRMN <DannielsR.MN@gmail.com>
2025-05-22 13:21:30 -0500

    Primer avance - Squizzable EMC

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardUsuarioComponent } from './dashboard-usuario/dashboard-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "Dashboard-Usuario", component: DashboardUsuarioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
